b3f3d0adb629272df3b6e4728f680556a956b01b
Dale Arney <Dale.C.Arney@nasa.gov>
2018-06-26 21:57:47 -0400

    Created the models with local MongoDB

--- models-test.py
+++ models-test.py
@@ -1,21 +1,21 @@
 import mongoengine as me
 
-me.connect('menumaker')
+me.connect('menumaker-test')
 
-class Recipe(me.EmbeddedDocument):
+class Recipe(me.Document):
     name = me.StringField()
     instructions = me.StringField()
     ingredients = me.ListField(me.StringField)
 
 class Menu(me.Document):
     name = me.StringField()
-    recipes = me.ListField(me.EmbeddedDocumentField(Recipe))
+    recipes = me.ListField(me.StringField)
 
 # Add data
 week1 = Menu(name='week1').save()
 spaghetti = Recipe(name='spaghetti', instructions='cook', ingredients=['noodles', 'spaghetti sauce', 'meatballs']).save()
 burgers = Recipe(name='burgers', instructions='grill', ingredients=['hamburgers', 'buns']).save()
-week1.recipes = [spaghetti, burgers]
+week1.recipes = ['spaghetti', 'burgers']
 week1.save()
 
 for recipe in Recipe.objects:
