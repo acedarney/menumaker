252c7b066d3cf4f4dc5840e7ea34bb64c3a10115
Dale Arney <Dale.C.Arney@nasa.gov>
2018-07-04 06:40:34 -0400

    Create nested ingredients list

--- menumaker-flask/app.py
+++ menumaker-flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, jsonify, request
 from flask_mongoengine import MongoEngine
+from flask_mongoengine.wtf import model_form
 from flask_marshmallow import Marshmallow
 from marshmallow_mongoengine import ModelSchema
 
@@ -11,12 +12,18 @@ db = MongoEngine(app)
 ma = Marshmallow(app)
 # db.init_app(app)
 
+# Create models
+
+class Ingredient(db.EmbeddedDocument):
+    name = db.StringField()
+    measure = db.StringField()
+    qty = db.StringField()
 
 class Recipe(db.Document):
     name = db.StringField()
     instructions = db.StringField()
-    # ingredients = db.ListField(db.StringField)
-    ingredients = db.StringField()
+    ingredients = db.ListField(db.EmbeddedDocumentField(Ingredient))
+    # ingredients = db.StringField()
 
 class Menu(db.Document):
     name = db.StringField()
@@ -31,7 +38,7 @@ class RecipeSchema(ModelSchema):
 recipe_schema = RecipeSchema()
 recipes_schema = RecipeSchema(many=True)
 
-# Create API (Create, Read, Update, Delete)
+# CRUD operations for API (Create, Read, Update, Delete)
 
 @app.route('/api/recipes')
 def get_recipes_api():
@@ -48,32 +55,33 @@ def get_recipe_api(id):
 @app.route('/api/add', methods=['POST'])
 def add_recipe_api():
     new_data = request.get_json(force=True)
-    new_recipe = Recipe(name=new_data['name'],
-                        instructions=new_data['instructions'],
-                        ingredients=new_data['ingredients'])
-    new_recipe.save()
+    new_recipe = recipe_schema.load(new_data)
+    # for ingredient in new_recipe.ingredients:
+    #     ingredient.data.save()
+    new_recipe.data.save()
     result = recipe_schema.dump(new_recipe)
-    return jsonify(new_data)
+    return jsonify(new_recipe)
     
+# Forms with WTForms
 
+# RecipeForm = model_form(Recipe)
+
+
+
+
+
+# Create Views (recipe list, recipe detail, menu list, menu detail)
 
 @app.route('/')
 def index():
     return 'Dale loves Jen'
 
-@app.route('/add')
+@app.route('/recipes/clear')
 def add():
     """
     Add a fixed document to the 'recipe' collection.  
     """
     Recipe.objects().delete()
-    Recipe(name='Spaghetti', 
-           instructions='Cook noodles. Add sauce.', 
-           ingredients='Noodles, Sauce').save()
-    Recipe(name='Spaghetti with Meatballs', 
-           instructions='Cook meatballs. Cook noodles. Add sauce and meatballs.', 
-           ingredients='Noodles, Sauce, Meatballs').save()
-    
     return redirect(url_for('view_recipes_list'))
 
 @app.route('/recipes')
@@ -85,7 +93,7 @@ def view_recipes_list():
     return render_template('recipe_list.html', recipes=recipes)
 
 @app.route('/recipe/<name>')
-def view_single_recipe(name):
+def view_recipe_detail(name):
     """
     View a single recipe
     """
